1d364298693979d2a3a86ce9d3d3f3624306c1fe
HuoHuas001 <2351078777@qq.com>
2021-08-11 21:07:55 +0800

    upload API

--- js/index.js
+++ js/index.js
@@ -13,3 +13,34 @@ function Jump_to_Github(){
 function Jump_to_Minebbs(){
     window.location.href='https://www.minebbs.com/resources/bdspyrunner-python.2079/'
 }
+
+function Jump_to_QQ(){
+    window.location.href='https://jq.qq.com/?_wv=1027&k=a1utBpEx'
+}
+
+//更改标题
+var homeBtn = document.getElementById('JHome');
+var APIBtn = document.getElementById('JApi')
+var Title = document.getElementById('title')
+homeBtn.onclick = function(){
+    Title.innerHTML = "PYR Wiki-Home"
+}
+APIBtn.onclick = function(){
+    Title.innerHTML = "PYR Wiki-API"
+}
+
+//显示API
+var apibtn = document.getElementsByClassName('APIBtn')
+var apis = document.getElementsByClassName('API')
+//添加索引
+for(var i=0;i<apis.length;i++){
+   apibtn[i].index = i
+   console.log(apibtn[i].index)
+   apibtn[i].onclick = function(){
+       for(var b=0;b<apis.length;b++){
+           apis[b].className = 'API no-show'
+       }
+       console.log(this.index)
+       apis[this.index].className = 'API show'
+   } 
+}
