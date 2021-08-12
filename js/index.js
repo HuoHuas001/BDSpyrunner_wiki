8e590f114546837a0cdf2d51447faf5f5aa6b84d
HuoHuas001 <2351078777@qq.com>
2021-08-12 11:00:44 +0800

    Uploade code

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 //获取菜单 c
 var inst = new mdui.Menu('#menuBtn', '#menu');
 var menuBtn = document.getElementById("menuBtn");
+var body = document.getElementById('Body')
 menuBtn.addEventListener("click", function(e){
     inst.open();
 });
@@ -30,15 +31,34 @@ APIBtn.onclick = function(){
 }
 
 //显示API
-var apibtn = document.getElementsByClassName('APIBtn')
+var $ = mdui.$;
 var apis = document.getElementsByClassName('API')
-//添加索引
-for(var i=0;i<apis.length;i++){
-   apibtn[i].index = i
-   apibtn[i].onclick = function(){
-       for(var b=0;b<apis.length;b++){
-           apis[b].className = 'API no-show'
-       }
-       apis[this.index].className = 'API show'
-   } 
-}
+$('#APIselect').on('close.mdui.select', function () {
+    var select_list = document.getElementById("APIselect")
+    var index=select_list.selectedIndex
+    for(var i = 0;i<apis.length;i++){
+        apis[i].className = 'API no-show'
+    }
+    apis[index].className = 'API show'
+});
+
+
+//显示页面
+var sidebar = document.getElementById('left')
+var main_content = document.getElementById('main')
+setInterval(function(){
+    if(body.clientWidth<600){
+        main_content.className = 'mdui-color-yellow-100 none-main-content'
+        indexBtn.style='display:block'
+    }
+    else{
+        main_content.className = 'mdui-color-yellow-100 block-main-content'
+        indexBtn.style='display:none'
+    }
+},0)
+
+var inst2 = new mdui.Menu('#indexBtn', '#index');
+var indexBtn = document.getElementById("indexBtn");
+indexBtn.addEventListener("click", function(e){
+    inst2.open();
+});
